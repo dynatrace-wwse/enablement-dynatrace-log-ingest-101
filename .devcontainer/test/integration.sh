e2f0b27e73104e5aec40d1917014f5adc39f041f
Sergio Hinojosa <hj.sergio@gmail.com>
2026-04-01 14:26:51 +0000

    fix integration tests
    Signed-off-by: Sergio Hinojosa <hj.sergio@gmail.com>

--- .devcontainer/test/integration.sh
+++ .devcontainer/test/integration.sh
@@ -3,12 +3,11 @@
 source .devcontainer/util/source_framework.sh
 
 printInfoSection "Running integration Tests for $RepositoryName"
-assertRunningPod astroshop adservice
 
-assertRunningPod astroshop cartservice
+assertRunningPod astroshop ad
 
-assertRunningPod astroshop checkoutservice
+assertRunningPod astroshop cart
 
-assertRunningPod cert-manager cainjector
+assertRunningPod astroshop checkout
 
 assertRunningApp 30100
